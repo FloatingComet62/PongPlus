2256ec402d2136bfaaa4a971642e0357e4629658
AarghCreate <floatingcomet62@gmail.com>
2022-06-22 22:31:01 +0530

    Cleaning

--- main.py
+++ main.py
@@ -4,36 +4,45 @@ from objects import Paddle, Ball
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    windowColor = (120, 120, 255)
     running = True
+    died = False
+    ballSpeed = 0.5
 
 
     paddle = Paddle(screen)
-    ball = Ball(screen)
+    ball = Ball(screen, ballSpeed)
+    score = 0
 
     def end():
         ball.y_speed = 0
         ball.x_speed = 0
         paddle.speed = 0
-        windowColor = (120, 0, 0)
+        return True
 
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            if event.type == pygame.QUIT: running = False
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            paddle.left()
-        elif keys[pygame.K_RIGHT]:
-            paddle.right()
+        if keys[pygame.K_LEFT]: paddle.left()
+        elif keys[pygame.K_RIGHT]: paddle.right()
+
+        if died:
+            windowColor = (255, 0, 0)
+            if keys[pygame.K_r]:
+                ball.reset(screen)
+                paddle.reset()
+                died = False
+                score = 0
+        else:
+            windowColor = (120, 120, 255)
 
         screen.fill(windowColor)
 
-        collision = ball.draw(paddle)
-        if collision:
-            end()
+        collision = ball.draw(paddle, score)
+        score = collision[1]
+        if collision[0]: died = end()
         paddle.draw()
 
         pygame.display.update()
-        pygame.display.set_caption(f"PongPlus")
+        pygame.display.set_caption(f"PongPlus: Your score is {score}")
--- objects.py
+++ objects.py
@@ -10,26 +10,28 @@ class Paddle(BaseEntity):
         self.width = 100
         self.height = 10
         self.screen = screen
-        self.speed = 1
+        self.speed = 0.5
     
         super().__init__(screen.get_width() / 2 - self.width / 2, 5)
     
-    def draw(self):
-        pygame.draw.rect(self.screen, (255, 255, 255), (self.x, self.y, self.width, self.height))
+    def draw(self): pygame.draw.rect(self.screen, (255, 255, 255), (self.x, self.y, self.width, self.height))
     
     def left(self):
-        if self. x + self.width > 0:
-            self.x -= self.speed
+        if self.x + self.width / 2 > 0: self.x -= self.speed
     
     def right(self):
-        if self.x + self.width < self.screen.get_width():
-            self.x += self.speed
+        if self.x + self.width < self.screen.get_width(): self.x += self.speed
+    
+    def reset(self):
+        self.speed = 1
+        self.width = 100
+        self.x = self.screen.get_width() / 2 - self.width / 2
 
 class Ball(BaseEntity):
-    def __init__(self, screen):
+    def __init__(self, screen, speed):
         self.radius = 10
-        self.x_speed = 0.5
-        self.y_speed = 0.5
+        self.x_speed = speed
+        self.y_speed = speed
         self.screen = screen
 
         super().__init__(screen.get_width() / 2, screen.get_height() / 2)
@@ -38,7 +40,7 @@ class Ball(BaseEntity):
         self.x += self.x_speed
         self.y += self.y_speed
     
-    def draw(self, paddle):
+    def draw(self, paddle, score):
         self.move()
         pygame.draw.circle(self.screen, (255, 255, 255), (self.x, self.y), self.radius)
 
@@ -46,14 +48,12 @@ class Ball(BaseEntity):
         if (
             self.x + self.radius >= self.screen.get_width() or
             self.x - self.radius <= 0
-        ):
-            self.x_speed *= -1
+        ): self.x_speed *= -1
 
         if (
             self.y + self.radius >= self.screen.get_height() or
             self.y - self.radius <= 0
-        ):
-            self.y_speed *= -1
+        ): self.y_speed *= -1
 
         # paddle collision
         if (
@@ -67,8 +67,16 @@ class Ball(BaseEntity):
                 self.y_speed *= -1
                 self.x_speed += 0.2
                 self.y_speed += 0.2
+                paddle.speed += 0.1
+                paddle.width -= 1
+                score+=1
+        
         # end check
-        if self.y - self.radius <= 0:
-            return True
-        else:
-            return False
+        if self.y - self.radius <= 0: return [ True, score ]
+        else: return [ False, score ]
+    
+    def reset(self, screen):
+        self.x = screen.get_width() / 2
+        self.y = screen.get_height() / 2
+        self.x_speed = 0.5
+        self.y_speed = 0.5
